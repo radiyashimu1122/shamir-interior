eeace0ac4a9453e94c1454e7e1bba6881f4a732f
radiyashimu1122 <radiyashimu@gmail.com>
2024-11-23 06:33:55 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("hamburger").onclick = function toggleMenu() {
+  const navToggle = document.getElementsByClassName("toggle");
+  for (let i = 0; i < navToggle.length; i++) {
+    navToggle.item(i).classList.toggle("hidden");
+  }
+}
+
+  function toggleContent(postNumber) {
+      const content = document.getElementById(`more-content-${postNumber}`);
+      const button = document.getElementById(`read-more-${postNumber}`);
+      
+      if (content.classList.contains('hidden')) {
+          content.classList.remove('hidden');
+          button.textContent = 'Read Less';
+      } else {
+          content.classList.add('hidden');
+          button.textContent = 'Read More';
+      }
+  }
+//   function toggleContent(postNumber) {
+//     const content = document.getElementById(`more-content-${postNumber}`);
+//     const button = document.getElementById(`read-more-${postNumber}`);
+//     if (content.classList.contains('hidden')) {
+//         content.classList.remove('hidden');
+//         button.textContent = 'Read Less';
+//     } else {
+//         content.classList.add('hidden');
+//         button.textContent = 'Read More';
+//     }
+// }
+
